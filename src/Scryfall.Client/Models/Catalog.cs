dd4a0fa5c2d3cee06315a68136832539862e4ad3
Sam Eden <samtteden@gmail.com>
2025-05-24 09:17:30 +0100

    Rename project

---
+++ src/Scryfall.Client/Models/Catalog.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Scryfall.Client.Models;
+
+class Catalog : BaseItem
+{
+    [JsonPropertyName("uri")]
+    public Uri Uri { get; set; }
+
+    [JsonPropertyName("total_values")]
+    public int TotalValues { get; set; }
+
+    [JsonPropertyName("data")]
+    public string[] Data { get; set; }
+}
