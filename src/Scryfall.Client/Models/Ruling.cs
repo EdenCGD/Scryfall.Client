01c555596b1d679eab8804346842b61db27b0e74
Sam Eden <samtteden@gmail.com>
2025-06-01 16:22:22 +0100

    Add ruling endpoints

---
+++ src/Scryfall.Client/Models/Ruling.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Scryfall.Client.Models
+{
+    public class Ruling : BaseItem
+    {
+        [JsonPropertyName("oracle_id")]
+        public string OracleId { get; set; }
+
+        [JsonPropertyName("source")]
+        public string Source { get; set; }
+
+        [JsonPropertyName("published_at")]
+        public DateTime Published { get; set; }
+
+        [JsonPropertyName("comment")]
+        public string Comment { get; set; }
+    }
+}
